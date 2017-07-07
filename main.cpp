a645b975fa8e2734c30e5b8b50381b7e953b7bf5
jwwangchn <jwwangchn@outlook.com>
2017-07-07 22:39:31 +0800

    finish read distance information

--- main.cpp
+++ main.cpp
@@ -11,38 +11,44 @@ int main()
     rs::context ctx;
     rs::device *dev = ctx.get_device(0);
 
-    if (ctx.get_device_count() <= 0)
+    if (ctx.get_device_count() == 0)
     {
-        cout << "can't open the realsense!!!" << endl;
+        cout << "No device detected!!!" << endl;
         return 0;
     }
 
-    dev->enable_stream(rs::stream::color, INPUT_WIDTH, INPUT_HEIGHT, rs::format::bgr8, FRAME_RATE);
+    dev->enable_stream(rs::stream::color, INPUT_WIDTH, INPUT_HEIGHT, rs::format::rgb8, FRAME_RATE);
     dev->enable_stream(rs::stream::depth, INPUT_WIDTH, INPUT_HEIGHT, rs::format::z16, FRAME_RATE);
 
     dev->start();
     while (1)
     {
         // Camera warmup - Dropped frames to allow stabilization
-        for (int i = 0; i < 1; i++)
+        for (int i = 0; i < 2; i++)
+        {
             dev->wait_for_frames();
+        }
+        uchar *pRgb = (uchar *)dev->get_frame_data(rs::stream::color);
+        uint16_t *depthImage = (uint16_t *)dev->get_frame_data(rs::stream::depth);
+        uchar *pCad = (uchar *)dev->get_frame_data(rs::stream::color_aligned_to_depth);
 
-        Mat bgr(Size(INPUT_WIDTH, INPUT_HEIGHT), CV_8UC3, (void *)dev->get_frame_data(rs::stream::color), Mat::AUTO_STEP);
-        Mat depth(Size(INPUT_WIDTH, INPUT_HEIGHT), CV_8UC1, (void *)dev->get_frame_data(rs::stream::depth), Mat::AUTO_STEP);
-        Mat cad(Size(INPUT_WIDTH, INPUT_HEIGHT), CV_8UC3, (void *)dev->get_frame_data(rs::stream::color_aligned_to_depth), Mat::AUTO_STEP);
+        Mat rgb_show;
+        Mat rgb(INPUT_HEIGHT, INPUT_WIDTH, CV_8UC3, pRgb);
+        cvtColor(rgb, rgb_show, CV_BGR2RGB);
+        Mat depth(INPUT_HEIGHT, INPUT_WIDTH, CV_16UC1, depthImage);
+        Mat cad_show;
+        Mat cad(INPUT_HEIGHT, INPUT_WIDTH, CV_8UC3, pCad);
+        cvtColor(cad, cad_show, CV_BGR2RGB);
 
-        // equalizeHist(depth, depth);
-        applyColorMap(depth, depth, COLORMAP_JET);
 
-        namedWindow("Display Image RGB", WINDOW_AUTOSIZE);
-        namedWindow("Display Image DEPTH", WINDOW_AUTOSIZE);
-        namedWindow("Display Image CAD", WINDOW_AUTOSIZE);
-        imshow("Display Image RGB", bgr);
-        imshow("Display Image DEPTH", depth);
-        imshow("Display Image CAD", cad);
+        imshow("RGBImage", rgb_show);
+        imshow("DepthImage", depth * 15);
+        imshow("CADImage", cad_show);
+        
+        for
+
         cout << getDistance(dev, 200, 200) << endl;
-        waitKey(1);
+        waitKey(10);
     }
-
     return 0;
 }
